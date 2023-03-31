3b4df8d5bdb5bf50b57b45d8e41dc399b967a06e
artem <artem.korobko@gmail.com>
2023-03-31 17:57:27 +0200

    Re-route data buses and add heap allocator

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,9 @@ mod peripherals;
 mod state;
 mod tasks;
 
+#[global_allocator]
+static HEAP: embedded_alloc::Heap = embedded_alloc::Heap::empty();
+
 #[rtic::app(device = stm32f1xx_hal::pac, dispatchers = [TAMPER, PVD, CAN_RX1, CAN_SCE])]
 mod app {
     use crate::peripherals::*;
@@ -37,10 +40,19 @@ mod app {
         trigger: gpio::PB13<gpio::Input<gpio::PullDown>>,
         sdmmc_detect_pin: gpio::PA3<gpio::Input<gpio::PullUp>>,
         sdmmc_detached_led: gpio::PA0<gpio::Output>,
+        sdmmc_write_led: gpio::PA1<gpio::Output>,
+        sdmmc_read_led: gpio::PA2<gpio::Output>,
     }
 
     #[init]
     fn init(mut cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        {
+            use core::mem::MaybeUninit;
+            const HEAP_SIZE: usize = 1024;
+            static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+            unsafe { super::HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+        }
+
         let mut afio = cx.device.AFIO.constrain();
         let mut flash = cx.device.FLASH.constrain();
         let rcc = cx.device.RCC.constrain();
@@ -61,62 +73,68 @@ mod app {
         let (pa15, _pb3, pb4) = afio.mapr.disable_jtag(gpioa.pa15, gpiob.pb3, gpiob.pb4);
 
         // Configure input bus pins
-        gpiob.pb6.into_pull_down_input(&mut gpiob.crl); // Data_Bus_Input_00
-        gpioe.pe0.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_01
-        gpiob.pb9.into_pull_down_input(&mut gpiob.crh); // Data_Bus_Input_02
-        gpioe.pe1.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_03
-        gpioe.pe2.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_04
-        gpioe.pe4.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_05
-        gpioe.pe3.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_06
-        gpioe.pe5.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_07
         pb4.into_pull_down_input(&mut gpiob.crl); // Data_Bus_Input_08
-        gpiod.pd6.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_09
-        gpiod.pd5.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_10
-        gpiod.pd7.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_11
-        gpiod.pd4.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_12
-        gpiod.pd1.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_13
-        gpiod.pd3.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_14
-        gpiod.pd0.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_15
-        gpiob.pb8.into_pull_down_input(&mut gpiob.crh); // Reset_Input
-        gpiod.pd2.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_Control_0
-        gpiob.pb7.into_pull_down_input(&mut gpiob.crl); // Data_Bus_Input_Control_1
-        gpiob.pb14.into_pull_down_input(&mut gpiob.crh); // Data_Transfer_Control_Completed_Input
+        gpiob.pb6.into_pull_down_input(&mut gpiob.crl); // Data_Transfer_Short_Input
+        gpiob.pb7.into_pull_down_input(&mut gpiob.crl); // Data_Bus_Input_00
+        gpiob.pb8.into_pull_down_input(&mut gpiob.crh); // Data_Bus_Input_Control_1
+        gpiob.pb9.into_pull_down_input(&mut gpiob.crh); // Reset_Input
         let mut trigger = gpiob.pb13.into_pull_down_input(&mut gpiob.crh); // Data_Transfer_Long_Input
         trigger.make_interrupt_source(&mut afio);
         trigger.enable_interrupt(&mut cx.device.EXTI);
         trigger.trigger_on_edge(&mut cx.device.EXTI, gpio::Edge::Falling);
-        gpiob.pb11.into_pull_down_input(&mut gpiob.crh); // Data_Transfer_Short_Input
-
+        gpiob.pb14.into_pull_down_input(&mut gpiob.crh); // Data_Transfer_Control_Completed_Input
+        gpiod.pd0.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_15
+        gpiod.pd1.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_13
+        gpiod.pd2.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_Control_0
+        gpiod.pd3.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_14
+        gpiod.pd4.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_12
+        gpiod.pd5.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_10
+        gpiod.pd6.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_09
+        gpiod.pd7.into_pull_down_input(&mut gpiod.crl); // Data_Bus_Input_11
+        gpioe.pe0.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_02
+        gpioe.pe1.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_01
+        gpioe.pe2.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_03
+        gpioe.pe3.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_04
+        gpioe.pe4.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_06
+        gpioe.pe5.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_05
+        gpioe.pe6.into_pull_down_input(&mut gpioe.crl); // Data_Bus_Input_07
+        
         // Configure output bus pins
-        gpioc.pc10.into_pull_down_input(&mut gpioc.crh); // Data_Bus_Output_00
-        gpioa.pa12.into_pull_down_input(&mut gpioa.crh); // Data_Bus_Output_01
-        gpioa.pa10.into_pull_down_input(&mut gpioa.crh); // Data_Bus_Output_02
         gpioa.pa8.into_pull_down_input(&mut gpioa.crh); // Data_Bus_Output_03
-        gpioc.pc9.into_pull_down_input(&mut gpioc.crh); // Data_Bus_Output_04
-        gpioc.pc8.into_pull_down_input(&mut gpioc.crh); // Data_Bus_Output_05
-        gpioc.pc7.into_pull_down_input(&mut gpioc.crl); // Data_Bus_Output_06
-        gpiod.pd15.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_07
-        gpiod.pd14.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_08
-        gpioc.pc6.into_pull_down_input(&mut gpioc.crl); // Data_Bus_Output_09
-        gpiod.pd13.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_10
-        gpiod.pd8.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_11
-        gpiod.pd11.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_12
-        gpiod.pd12.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_13
+        gpioa.pa9.into_pull_down_input(&mut gpioa.crh); // Data_Bus_Output_Control_1
+        gpioa.pa10.into_pull_down_input(&mut gpioa.crh); // Data_Bus_Output_02
+        gpioa.pa11.into_pull_down_input(&mut gpioa.crh); // Data_Bus_Output_Control_0
+        gpioa.pa12.into_pull_down_input(&mut gpioa.crh); // Data_Bus_Output_01
+        pa15.into_pull_down_input(&mut gpioa.crh); // External_Error_Output
+        gpiob.pb12.into_pull_down_input(&mut gpiob.crh); // External_Reset_Output
         gpiob.pb15.into_pull_down_input(&mut gpiob.crh); // Data_Bus_Output_14
+        gpioc.pc3.into_pull_down_input(&mut gpioc.crl); // External_Set_Output
+        gpioc.pc6.into_pull_down_input(&mut gpioc.crl); // Data_Bus_Output_09
+        gpioc.pc7.into_pull_down_input(&mut gpioc.crl); // Data_Bus_Output_06
+        gpioc.pc8.into_pull_down_input(&mut gpioc.crh); // Data_Bus_Output_05
+        gpioc.pc9.into_pull_down_input(&mut gpioc.crh); // Data_Bus_Output_04
+        gpioc.pc10.into_pull_down_input(&mut gpioc.crh); // Data_Bus_Output_00
+        gpioc.pc11.into_pull_down_input(&mut gpioc.crh); // Data_Transfer_Completed_Output
+        gpioc.pc12.into_pull_down_input(&mut gpioc.crh); // Data_Bus_Output_Control_State
+        gpiod.pd8.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_11
         gpiod.pd9.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_15
         gpiod.pd10.into_pull_down_input(&mut gpiod.crh); // Ready_Output
-        gpioe.pe6.into_pull_down_input(&mut gpioe.crl); // External_Set_Output
-        gpiob.pb12.into_pull_down_input(&mut gpiob.crh); // External_Reset_Output
-        pa15.into_pull_down_input(&mut gpioa.crh); // External_Error_Output
-        gpioc.pc12.into_pull_down_input(&mut gpioc.crh); // Data_Bus_Output_Control_State
-        gpioa.pa11.into_pull_down_input(&mut gpioa.crh); // Data_Bus_Output_Control_0
-        gpioa.pa9.into_pull_down_input(&mut gpioa.crh); // Data_Bus_Output_Control_1
-        gpioc.pc11.into_pull_down_input(&mut gpioc.crh); // Data_Transfer_Completed_Output
+        gpiod.pd11.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_12
+        gpiod.pd12.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_13
+        gpiod.pd13.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_10
+        gpiod.pd14.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_08
+        gpiod.pd15.into_pull_down_input(&mut gpiod.crh); // Data_Bus_Output_07
 
         // Configure LED indicators
         let sdmmc_detached_led = gpioa
             .pa0
             .into_push_pull_output_with_state(&mut gpioa.crl, gpio::PinState::Low);
+        let sdmmc_write_led = gpioa
+            .pa1
+            .into_push_pull_output_with_state(&mut gpioa.crl, gpio::PinState::High);
+        let sdmmc_read_led = gpioa
+            .pa2
+            .into_push_pull_output_with_state(&mut gpioa.crl, gpio::PinState::High);
 
         // Configure SDMMC
         let sdmmc_detect_pin = gpioa.pa3.into_pull_up_input(&mut gpioa.crl);
@@ -151,6 +169,8 @@ mod app {
                 trigger,
                 sdmmc_detect_pin,
                 sdmmc_detached_led,
+                sdmmc_write_led,
+                sdmmc_read_led,
             },
             init::Monotonics(),
         )
--- src/peripherals/sm2m.rs
+++ src/peripherals/sm2m.rs
@@ -6,8 +6,6 @@ pub struct InputState {
     pub ctrl0: bool,
     pub ctrl1: bool,
     pub dtc: bool,
-    pub dtl: bool,
-    pub dts: bool,
 }
 
 impl InputState {
@@ -18,8 +16,6 @@ impl InputState {
             ctrl0: !self.ctrl0,
             ctrl1: !self.ctrl1,
             dtc: !self.dtc,
-            dtl: !self.dtl,
-            dts: !self.dts,
         }
     }
 }
@@ -30,10 +26,10 @@ pub struct OutputState {
     pub ctrl: bool,
     pub ctrl0: bool,
     pub ctrl1: bool,
-    pub exte: bool,
+    pub ext_err: bool,
     pub complete: bool,
     pub ready: bool,
-    pub set: bool,
+    pub ext_set: bool,
 }
 
 impl OutputState {
@@ -44,10 +40,10 @@ impl OutputState {
             ctrl: !self.ctrl,
             ctrl0: !self.ctrl0,
             ctrl1: !self.ctrl1,
-            exte: !self.exte,
+            ext_err: !self.ext_err,
             complete: !self.complete,
             ready: !self.ready,
-            set: !self.set,
+            ext_set: !self.ext_set,
         }
     }
 }
@@ -78,12 +74,12 @@ impl Bus {
 
         self.device.GPIOA.odr.modify(|r, w| {
             let mut bits = r.bits();
-            bits = write_bit(bits, 12, data, 1); // pa12 - Data_Bus_Output_01
-            bits = write_bit(bits, 10, data, 2); // pa10 - Data_Bus_Output_02
             bits = write_bit(bits, 8, data, 3); // pa8 - Data_Bus_Output_03
-            bits = write_flag(bits, 15, state.exte); // pa15 - External_Error_Output
-            bits = write_flag(bits, 11, state.ctrl0); // pa11 - Data_Bus_Output_Control_0
             bits = write_flag(bits, 9, state.ctrl1); // pa9 - Data_Bus_Output_Control_1
+            bits = write_bit(bits, 10, data, 2); // pa10 - Data_Bus_Output_02
+            bits = write_flag(bits, 11, state.ctrl0); // pa11 - Data_Bus_Output_Control_0
+            bits = write_bit(bits, 12, data, 1); // pa12 - Data_Bus_Output_01
+            bits = write_flag(bits, 15, state.ext_err); // pa15 - External_Error_Output
             w.bits(bits)
         });
 
@@ -96,32 +92,27 @@ impl Bus {
 
         self.device.GPIOC.odr.modify(|r, w| {
             let mut bits = r.bits();
-            bits = write_bit(bits, 10, data, 0); // pc10 - Data_Bus_Output_00
-            bits = write_bit(bits, 9, data, 4); // pc9 - Data_Bus_Output_04
-            bits = write_bit(bits, 8, data, 5); // pc8 - Data_Bus_Output_05
-            bits = write_bit(bits, 7, data, 6); // pc7 - Data_Bus_Output_06
+            bits = write_flag(bits, 3, state.ext_set); // pc3 - External_Set_Output
             bits = write_bit(bits, 6, data, 9); // pc6 - Data_Bus_Output_09
-            bits = write_flag(bits, 12, state.ctrl); // pc12 - Data_Bus_Output_Control_State
+            bits = write_bit(bits, 7, data, 6); // pc7 - Data_Bus_Output_06
+            bits = write_bit(bits, 8, data, 5); // pc8 - Data_Bus_Output_05
+            bits = write_bit(bits, 9, data, 4); // pc9 - Data_Bus_Output_04
+            bits = write_bit(bits, 10, data, 0); // pc10 - Data_Bus_Output_00
             bits = write_flag(bits, 11, state.complete); // pc11 - Data_Transfer_Completed_Output
+            bits = write_flag(bits, 12, state.ctrl); // pc12 - Data_Bus_Output_Control_State
             w.bits(bits)
         });
 
         self.device.GPIOD.odr.modify(|r, w| {
             let mut bits = r.bits();
-            bits = write_bit(bits, 15, data, 7); // pd15 - Data_Bus_Output_07
-            bits = write_bit(bits, 14, data, 8); // pd14 - Data_Bus_Output_08
-            bits = write_bit(bits, 13, data, 10); // pd13 - Data_Bus_Output_10
             bits = write_bit(bits, 8, data, 11); // pd8 - Data_Bus_Output_11
-            bits = write_bit(bits, 11, data, 12); // pd11 - Data_Bus_Output_12
-            bits = write_bit(bits, 12, data, 13); // pd12 - Data_Bus_Output_13
             bits = write_bit(bits, 9, data, 15); // pd9 - Data_Bus_Output_15
             bits = write_flag(bits, 10, state.ready); // pd10 - Ready_Output
-            w.bits(bits)
-        });
-
-        self.device.GPIOE.odr.modify(|r, w| {
-            let mut bits = r.bits();
-            bits = write_flag(bits, 6, state.set); // pe6 - External_Set_Output
+            bits = write_bit(bits, 11, data, 12); // pd11 - Data_Bus_Output_12
+            bits = write_bit(bits, 12, data, 13); // pd12 - Data_Bus_Output_13
+            bits = write_bit(bits, 13, data, 10); // pd13 - Data_Bus_Output_10
+            bits = write_bit(bits, 14, data, 8); // pd14 - Data_Bus_Output_08
+            bits = write_bit(bits, 15, data, 7); // pd15 - Data_Bus_Output_07
             w.bits(bits)
         });
     }
@@ -144,29 +135,27 @@ impl Bus {
         let pd = self.device.GPIOD.idr.read().bits() as u16;
         let pe = self.device.GPIOE.idr.read().bits() as u16;
 
-        let data = copy_bit(pb, 6, 0) // pb6 - Data_Bus_Input_00
-        | copy_bit(pe, 0, 1) // pe0 - Data_Bus_Input_01
-        | copy_bit(pb, 9, 2) // pb9 - Data_Bus_Input_02
-        | copy_bit(pe, 1, 3) // pe1 - Data_Bus_Input_03
-        | copy_bit(pe, 2, 4) // pe2 - Data_Bus_Input_04
-        | copy_bit(pe, 4, 5) // pe4 - Data_Bus_Input_05
-        | copy_bit(pe, 3, 6) // pe3 - Data_Bus_Input_06
-        | copy_bit(pe, 5, 7) // pe5 - Data_Bus_Input_07
-        | copy_bit(pb, 4, 8) // pb4 - Data_Bus_Input_08
-        | copy_bit(pd, 6, 9) // pd6 - Data_Bus_Input_09
-        | copy_bit(pd, 5, 10) // pd5 - Data_Bus_Input_10
-        | copy_bit(pd, 7, 11) // pd7 - Data_Bus_Input_11
-        | copy_bit(pd, 4, 12) // pd4 - Data_Bus_Input_12
+        let data = copy_bit(pb, 4, 8) // pb4 - Data_Bus_Input_08
+        | copy_bit(pb, 7, 0) // pb7 - Data_Bus_Input_00
+        | copy_bit(pd, 0, 15) // pd0 - Data_Bus_Input_15
         | copy_bit(pd, 1, 13) // pd1 - Data_Bus_Input_13
         | copy_bit(pd, 3, 14) // pd3 - Data_Bus_Input_14
-        | copy_bit(pd, 0, 15); // pd0 - Data_Bus_Input_15
+        | copy_bit(pd, 4, 12) // pd4 - Data_Bus_Input_12
+        | copy_bit(pd, 5, 10) // pd5 - Data_Bus_Input_10
+        | copy_bit(pd, 6, 9) // pd6 - Data_Bus_Input_09
+        | copy_bit(pd, 7, 11) // pd7 - Data_Bus_Input_11
+        | copy_bit(pe, 0, 2) // pe0 - Data_Bus_Input_02
+        | copy_bit(pe, 1, 1) // pe1 - Data_Bus_Input_01
+        | copy_bit(pe, 2, 3) // pe2 - Data_Bus_Input_03
+        | copy_bit(pe, 3, 4) // pe3 - Data_Bus_Input_04
+        | copy_bit(pe, 4, 6) // pe4 - Data_Bus_Input_06
+        | copy_bit(pe, 5, 5) // pe5 - Data_Bus_Input_05
+        | copy_bit(pe, 6, 7); // pe6 - Data_Bus_Input_07
 
-        let reset = bit_is_set(pb, 8); // pb8 - Reset_Input
+        let reset = bit_is_set(pb, 9); // pb9 - Reset_Input
         let ctrl0 = bit_is_set(pd, 2); // pd2 - Data_Bus_Input_Control_0
-        let ctrl1 = bit_is_set(pb, 7); // pb7 - Data_Bus_Input_Control_1
+        let ctrl1 = bit_is_set(pb, 8); // pb8 - Data_Bus_Input_Control_1
         let dtc = bit_is_set(pb, 14); // pb14 - Data_Transfer_Control_Completed_Input
-        let dtl = bit_is_set(pb, 13); // pb13 - Data_Transfer_Long_Input
-        let dts = bit_is_set(pb, 11); // pb11 - Data_Transfer_Short_Input
 
         InputState {
             data,
@@ -174,8 +163,6 @@ impl Bus {
             ctrl0,
             ctrl1,
             dtc,
-            dtl,
-            dts,
         }
     }
 }
