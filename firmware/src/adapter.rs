d16caae8a62e5604c0f1b099d2621e094506b0c8
artem <artem.korobko@gmail.com>
2023-05-11 20:55:28 +0200

    Write buffer to SDMMC

--- firmware/src/adapter.rs
+++ firmware/src/adapter.rs
@@ -1,5 +1,3 @@
-use heapless::Vec;
-
 use crate::{
     error::AppError,
     peripherals::{
@@ -10,9 +8,9 @@ use crate::{
 
 enum Mode {
     Ready,
-    Address(u16),
-    Read(u16),
-    Write(u16),
+    Address,
+    Read,
+    Write,
     Error(u16),
 }
 
@@ -23,7 +21,9 @@ pub struct Device {
     output: output::Bus,
     card: sdmmc::Card,
     mode: Mode,
-    buf: Vec<u8, IO_BUFFER_SIZE>,
+    file_name: sdmmc::FileName,
+    buf: [u8; IO_BUFFER_SIZE],
+    buf_pos: usize,
 }
 
 impl Device {
@@ -33,16 +33,18 @@ impl Device {
             output,
             card,
             mode: Mode::Ready,
-            buf: Vec::new(),
+            file_name: sdmmc::FileName::new(),
+            buf: [0; IO_BUFFER_SIZE],
+            buf_pos: 0,
         }
     }
 
     pub fn run(&mut self) {
         let action = self.input.read();
-        self.execute_action(action);
+        self.execute(action);
     }
 
-    fn execute_action(&mut self, action: input::Action) {
+    fn execute(&mut self, action: input::Action) {
         match action {
             input::Action::Reset => self.handle_reset(),
             input::Action::End => self.handle_end(),
@@ -52,12 +54,17 @@ impl Device {
 
     fn handle_reset(&mut self) {
         defmt::println!("Handle reset");
+        self.buf_pos = 0;
         self.mode = Mode::Ready;
         self.output.write(output::Frame::Ack);
     }
 
     fn handle_end(&mut self) {
         defmt::println!("Handle end");
+        if self.buf_pos > 0 {
+            self.handle_dump_payload();
+        }
+
         self.handle_reset();
         self.output.write(output::Frame::Ack);
     }
@@ -69,13 +76,13 @@ impl Device {
                 input::Frame::Address(address) => self.handle_address(address),
                 _ => self.handle_error(AppError::UnhandledReadyCommand),
             },
-            Mode::Address(address) => match input::Frame::from(payload) {
-                input::Frame::Read => self.handle_read(address),
-                input::Frame::Write => self.handle_write(address),
+            Mode::Address => match input::Frame::from(payload) {
+                input::Frame::Read => self.handle_read(),
+                input::Frame::Write => self.handle_write(),
                 _ => self.handle_error(AppError::UnhandledAddressCommand),
             },
-            Mode::Read(address) => self.handle_read_payload(address),
-            Mode::Write(address) => self.handle_write_payload(address, payload),
+            Mode::Read => self.handle_read_payload(),
+            Mode::Write => self.handle_write_payload(payload),
             Mode::Error(opcode) => self.handle_error(opcode),
         }
     }
@@ -91,30 +98,58 @@ impl Device {
 
     fn handle_address(&mut self, address: u16) {
         defmt::println!("Handle address: {}", address);
-        self.mode = Mode::Address(address);
+        self.mode = Mode::Address;
+        self.file_name = sdmmc::FileName::from(address);
         self.output.write(output::Frame::Ack);
     }
 
-    fn handle_read(&mut self, address: u16) {
+    fn handle_read(&mut self) {
         defmt::println!("Handle read");
-        self.mode = Mode::Read(address);
+        self.mode = Mode::Read;
         self.output.write(output::Frame::Ack);
     }
 
-    fn handle_write(&mut self, address: u16) {
+    fn handle_write(&mut self) {
         defmt::println!("Handle write");
-        self.mode = Mode::Write(address);
+        // backup file
+        // remove file
+        self.mode = Mode::Write;
         self.output.write(output::Frame::Ack);
     }
 
-    fn handle_read_payload(&mut self, address: u16) {
-        defmt::println!("Handle read payload from address {}", address);
+    fn handle_read_payload(&mut self) {
+        defmt::println!("Handle read payload from {}", self.file_name.as_str());
         self.output.write(output::Frame::Data(0));
     }
 
-    fn handle_write_payload(&mut self, address: u16, payload: u16) {
-        defmt::println!("Handle write payload {} to address {}", payload, address);
-        self.output.write(output::Frame::Ack);
+    fn handle_write_payload(&mut self, payload: u16) {
+        if self.buf_pos + 2 <= self.buf.len() {
+            defmt::println!("Cache payload {} of {}", self.buf_pos, self.buf.len());
+            let bytes = payload.to_le_bytes();
+            self.buf[self.buf_pos + 1] = bytes[0];
+            self.buf[self.buf_pos + 2] = bytes[1];
+            self.buf_pos += 2;
+            self.output.write(output::Frame::Ack)
+        } else {
+            self.handle_dump_payload();
+        }
+    }
+
+    fn handle_dump_payload(&mut self) {
+        defmt::println!("Dump buf to {}", &self.file_name.as_str());
+        match self.write_buf_to_card() {
+            Ok(_) => {
+                self.buf_pos = 0;
+                self.output.write(output::Frame::Ack)
+            }
+            Err(error) => self.handle_error(error),
+        }
+    }
+
+    fn write_buf_to_card(&mut self) -> Result<usize, AppError> {
+        let mut controller = self.card.open()?;
+        let mut file = controller.oped_file_append(&self.file_name)?;
+        controller.write(&mut file, &self.buf[0..self.buf_pos])
     }
 
     fn handle_error<T: Into<u16>>(&mut self, error: T) {
--- firmware/src/peripherals/sdmmc/file.rs
+++ firmware/src/peripherals/sdmmc/file.rs
@@ -1,15 +1,17 @@
 use heapless::String;
 
+pub type FileName = String<5>;
+
 pub trait AsFileName {
-    fn as_file_name(&self) -> String<5>;
+    fn as_file_name(self) -> String<5>;
 }
 
 impl AsFileName for u16 {
-    fn as_file_name(&self) -> String<5> {
+    fn as_file_name(self) -> String<5> {
         const MAX_CHARS: usize = 10;
         const CHARS: [char; MAX_CHARS] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
         let mut file_name = String::new();
-        let mut num = *self as usize;
+        let mut num = self as usize;
 
         while num > 0 {
             let digit = core::cmp::min(MAX_CHARS - 1, num % 10);
